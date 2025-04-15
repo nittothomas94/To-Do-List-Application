867e91aabf79089b712f5f7f01e0a0a6da9fccc4
nittothomas <nittothomas94@example.com>
2025-04-15 17:35:10 +0530

    edited

--- frontend/src/pages/UserPages/Signin.jsx
+++ frontend/src/pages/UserPages/Signin.jsx
@@ -1,14 +1,30 @@
 import { Button, TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
 const Signin = () => {
   const navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  console.log(user);
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-100 via-blue-400 to-blue-50 flex items-center justify-center">
       <div className="min-h-150 w-150 bg-white flex flex-col gap-7 p-10">
         <h1 className="text-4xl font-sans font-bold">Login to your account</h1>
-        <TextField required id="outlined-required" placeholder="Name" />
-        <TextField required placeholder="Email" />
-        <TextField required placeholder="Password" />
+        <TextField
+          required
+          placeholder="Email"
+          onChange={e => setUser({ ...user, email: e.target.value })}
+        />
+        <TextField
+          required
+          placeholder="Password"
+          onChange={e => setUser({ ...user, password: e.target.value })}
+        />
         <Button
           variant="contained"
           className="w-full flex items-center content-center active:scale-95"
@@ -17,8 +33,13 @@ const Signin = () => {
           Sign in
         </Button>
         <p>
-          New here?{' '}
-          <span className="text-blue-600 hover:cursor-pointer">
+          New here?
+          <span
+            onClick={() => {
+              navigate('/signup');
+            }}
+            className="text-blue-600 hover:cursor-pointer"
+          >
             create an account
           </span>
         </p>
--- frontend/src/pages/UserPages/Signup.jsx
+++ frontend/src/pages/UserPages/Signup.jsx
@@ -1,15 +1,28 @@
 import { Button, TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Signup = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
   const navigate = useNavigate();
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-100 via-blue-400 to-blue-50 flex items-center justify-center">
       <div className="min-h-150 w-150 bg-white flex flex-col gap-7 p-10">
         <h1 className="text-4xl font-sans font-bold">Create your account</h1>
         <TextField required id="outlined-required" placeholder="Name" />
-        <TextField required placeholder="Email" />
-        <TextField required placeholder="Email" />
+        <TextField
+          required
+          placeholder="Email"
+          onChange={e => setUser({ ...user, email: e.target.value })}
+        />
+        <TextField
+          required
+          placeholder="Email"
+          onChange={e => setUser({ ...user, password: e.target.value })}
+        />
         <Button
           variant="contained"
           className="w-full flex items-center content-center active:scale-95"
@@ -19,7 +32,14 @@ const Signup = () => {
         </Button>
         <p>
           Already have an account ?{' '}
-          <span className="text-blue-600 hover:cursor-pointer">Sign in</span>
+          <span
+            onClick={() => {
+              navigate('/signin');
+            }}
+            className="text-blue-600 hover:cursor-pointer"
+          >
+            Sign in
+          </span>
         </p>
       </div>
     </div>
